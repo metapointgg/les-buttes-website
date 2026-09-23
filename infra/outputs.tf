d519b50c5fd1a431a4f7301fa5e1578bb8228244
Mike O <93396632+metapointgg@users.noreply.github.com>
2026-09-23 21:44:45 +0100

    Fix Terraform HCL block syntax

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,5 +1,19 @@
-output "site_bucket" { value=aws_s3_bucket.site.id }
-output "cloudfront_distribution_id" { value=aws_cloudfront_distribution.site.id }
-output "cloudfront_domain_name" { value=aws_cloudfront_distribution.site.domain_name }
-output "site_url" { value="https://${aws_cloudfront_distribution.site.domain_name}" }
-output "github_deploy_role_arn" { value=aws_iam_role.github.arn }
+output "site_bucket" {
+  value = aws_s3_bucket.site.id
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.site.id
+}
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.site.domain_name
+}
+
+output "site_url" {
+  value = "https://${aws_cloudfront_distribution.site.domain_name}"
+}
+
+output "github_deploy_role_arn" {
+  value = aws_iam_role.github.arn
+}
